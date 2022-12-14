d896c1271b388a4c19bf58172cd92f58f19e7abc
Dale Roberts <dsroberts@unimelb.edu.au>
2022-12-14 22:29:42 +1100

    Start work on initialise and install scripts

--- scripts/initialise.sh
+++ scripts/initialise.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env bash
 
+### Run me inside a singularity container launched with the following command:
+###
+### mkdir ${PBS_JOBFS}/overlay
+### /opt/singularity/bin/singularity -s exec --bind /etc,/haoot,/local,/ram,/run,/system,/usr,/var/lib/sss,/var/run/munge,$PBS_JOBFS/overlay:/g /g/data/v45/dr4292/singularity/test.sif initialise.sh
+### The important part is the $PBS_JOBFS/overlay:/g mount - we need an empty /g/data for this
+
+export OWD=${PWD}
+export CONDA_BASE=/g/data/v45/dr4292/conda_concept
+
+export CONDA_INSTALLATION_PATH=${CONDA_INSTALLATION_PATH:-$CONDA_BASE/apps/miniconda3}
+mkdir -p "${CONDA_INSTALLATION_PATH%/*}"
 ### Placeholder for script that will initialise the base conda environment from scratch
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b -p "${CONDA_INSTALLATION_PATH}"
+
+. "${CONDA_INSTALLATION_PATH}"/etc/profile.d/conda.sh
+conda install mamba -y
+
+export CONDA_SCRIPT_PATH="${CONDA_BASE}"/scripts
+export CONDA_MODULE_PATH="${CONDA_BASE}"/modules
+
+mkdir -p "${CONDA_SCRIPT_PATH}"/overrides
+cp "${OWD}"/launcher{,_conf}.sh "${CONDA_SCRIPT_PATH}"
+cp "${OWD}"/overrides/* "${CONDA_SCRIPT_PATH}"/overrides
+
+mkdir -p "${CONDA_MODULE_PATH}"/conda
+cp "${OWD}"/condaenv.sh "${CONDA_MODULE_PATH}"/conda
+cp "${OWD}"/../modules/common_v3 "${CONDA_MODULE_PATH}"/conda/.common_v3
+
+conda clean -a -f -y
+
+pushd "${CONDA_BASE}"
+tar -cf "${OWD}"/conda_base.tar apps modules scripts
+popd
+
+### Then untar into the real ${CONDA_INSTALLATION_PATH}
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,79 @@
 #!/usr/bin/env bash
 
-### Placeholder for script that will install/update a containerised conda environment
+### Run me inside a singularity container launched with the following command:
+###
+### mkdir -p $PBS_JOBFS/overlay
+### /opt/singularity/bin/singularity -s exec --bind /etc,/half-root,/local,/ram,/run,/system,/usr,/var/lib/sss,/var/run/munge,/var/lib/rpm,$PBS_JOBFS/overlay:/g /g/data/v45/dr4292/singularity/test.sif initialise.sh
+### The important part is the $PBS_JOBFS:/g mount - we need an empty /g/data for this
+
+export CONDA_BASE=/g/data/v45/dr4292/conda_concept
+export FULLENV=analysis3-22.12
+declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
+declare -a replace_from_apps=( "openmpi/4.1.4" )
+
+export OWD="${OWD:-/home/563/dr4292/cms-conda-singularity/scripts}"
+export CONDA_INSTALLATION_PATH=${CONDA_INSTALLATION_PATH:-${CONDA_BASE}/apps/miniconda3}
+export ENV_INSTALLATION_PATH="${PBS_JOBFS}"/squashfs-root/opt/conda/"${FULLENV}"
+export MAMBA="${CONDA_INSTALLATION_PATH}"/condabin/mamba
+
+mkdir -p "${ENV_INSTALLATION_PATH}"
+mkdir -p "${CONDA_INSTALLATION_PATH%/*}"
+
+pushd "${CONDA_BASE}"
+
+tar -cf "${OWD}"/conda_base.tar
+ln -s "${ENV_INSTALLATION_PATH}" apps/miniconda3/envs/
+
+popd
+
+source "${CONDA_INSTALLATION_PATH}"/etc/profile.d/conda.sh
+### Create the environment
+${MAMBA} env create -p "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}" -f environment.yml
+
+### Create symlink tree
+mkdir -p "${CONDA_BASE}"/scripts/"${FULLENV}".d/{bin,overrides}
+cp "${CONDA_BASE}"/scripts/{launcher.sh,launcher_conf.sh} "${CONDA_BASE}"/scripts/"${FULLENV}".d/bin
+pushd "${CONDA_BASE}"/scripts/"${FULLENV}".d/bin
+for i in $( ls "${ENV_INSTALLATION_PATH}"/bin ); do
+    ln -s launcher.sh $i
+done
+popd
+
+pushd "${CONDA_BASE}"/scripts/"${FULLENV}".d/overrides
+for i in ../../overrides/*; do
+    ln -s ${i}
+done
+popd
+
+pushd "${ENV_INSTALLATION_PATH}"
+### Get rid of stuff from packages we don't want
+for dir in bin lib etc libexec include; do 
+        pushd $dir
+        for i in $( rpm -qli "${rpms_to_remove[@]}" ); do 
+                fn=$( basename $i )
+                [[ -f $fn ]] && rm $fn
+                [[ -d $fn ]] && rm -rf $fn
+        done
+        popd
+done
+
+### Replace things from apps
+for pkg in "${replace_from_apps[@]}"; do
+    for dir in bin etc lib include; do 
+            pushd $dir 
+            for i in $( find /apps/$pkg/$dir -maxdepth 1 -type f ); do 
+                    fn=$( basename $i ) 
+                    [[ -e $fn ]] && rm $fn && ln -s $i
+            done
+            popd
+    done
+done
+popd
+
+set +u
+conda activate "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}"
+set -u
+
+py.test -s
+# Refresh jupyter plugins
+jupyter lab build
