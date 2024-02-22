c5c9d8858755987a15679a71a967a3d0329b7dbd
rbeucher <romain.beucher@anu.edu.au>
2024-02-22 12:51:49 +1100

    Test

--- environments/esmvaltool/build_inner.sh
+++ environments/esmvaltool/build_inner.sh
@@ -7,17 +7,17 @@ set -u
 
 jupyter lab build
 
-pushd ${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/lib/python3.11/site-packages/esmvaltool
+pushd /opt/conda/esmvaltool-0.1/lib/python3.11/site-packages/esmvaltool
 rm config-references.yml
 ln -sf /g/data/xp65/public/apps/esmvaltool/config/config-references.yml config-references.yml
 pushd -
 
-pushd ${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/lib/python3.11/site-packages/esmvalcore
+pushd /opt/conda/esmvaltool-0.1/lib/python3.11/site-packages/esmvalcore
 rm config-user.yml
 ln -sf /g/data/xp65/public/apps/esmvaltool/config/config-user.yml config-user.yml
 pushd -
 
-pushd ${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/lib/python3.11/site-packages/esmvalcore
+pushd /opt/conda/esmvaltool-0.1/lib/python3.11/site-packages/esmvalcore
 rm config-developer.yml
 ln -sf /g/data/xp65/public/apps/esmvaltool/config/config-developer.yml config-developer.yml
 pushd -
