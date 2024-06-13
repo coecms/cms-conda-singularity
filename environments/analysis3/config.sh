6978126aaa63aa24d02b8e5d78858f5c8b388b02
Dale Roberts <dsroberts@unimelb.edu.au>
2024-06-13 10:49:28 +1000

    Updates required for full rebuild of analysis3-24.04

--- environments/analysis3/config.sh
+++ environments/analysis3/config.sh
@@ -17,6 +17,7 @@ export ENVIRONMENT=analysis3
 export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
 
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
-declare -a replace_from_apps=( "openmpi/4.1.5" "ucx/1.14.0" )
+declare -a replace_from_apps=( "ucx/1.15.0" )
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" )
+declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" "/g/data/hh5/public/apps/openmpi/4.1.6" )
+declare -a replace_with_external=( "/g/data/hh5/public/apps/openmpi/4.1.6" )
