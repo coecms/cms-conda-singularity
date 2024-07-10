04b2227f9087ce8800fa8d44a8c4e779aacb2f68
rbeucher <romain.beucher@anu.edu.au>
2024-07-11 09:15:08 +1000

    Change config

--- environments/esmvaltool/config.sh
+++ environments/esmvaltool/config.sh
@@ -18,6 +18,7 @@ export ENVIRONMENT=esmvaltool
 export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
 
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
-declare -a replace_from_apps=( "openmpi/4.1.5" "ucx/1.14.0" )
+declare -a replace_from_apps=( "ucx/1.15.0" )
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" )
+declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" "/g/data/hh5/public/apps/openmpi/4.1.6" )
+declare -a replace_with_external=( "/g/data/hh5/public/apps/openmpi/4.1.6" )
