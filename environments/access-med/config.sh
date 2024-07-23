5d03e4f2afb687fbb0e97f4be897919bea2496a4
rbeucher <romain.beucher@anu.edu.au>
2024-07-23 15:24:39 +1000

    Build new environment for ILAMB workflow

--- environments/access-med/config.sh
+++ environments/access-med/config.sh
@@ -9,15 +9,16 @@
 
 
 ### Optional config for custom deploy script
-export VERSION_TO_MODIFY=0.7
+export VERSION_TO_MODIFY=0.8
 export STABLE_VERSION=0.3
-export UNSTABLE_VERSION=0.7
+export UNSTABLE_VERSION=0.8
 
 ### Version settings
-export ENVIRONMENT=access-med
+export ENVIRONMENT=esmvaltool
 export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
 
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
-declare -a replace_from_apps=( "openmpi/4.1.5" "ucx/1.14.0" )
+declare -a replace_from_apps=( "ucx/1.15.0" )
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" )
+declare -a outside_files_to_copy=( "/g/data/hh5/public/apps/nci-intake-catalogue/catalogue_new.yaml" "/g/data/hh5/public/apps/openmpi/4.1.6" )
+declare -a replace_with_external=( "/g/data/hh5/public/apps/openmpi/4.1.6" )
